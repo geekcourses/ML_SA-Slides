732a4f3902ac3dc149c2e78ed4576663eed7e314
geekcourses <geekcourses@gmail.com>
2022-07-13 15:34:33 +0300

    update

--- pages/themes/Python_For_ML_1_2/examples/tmp.py
+++ pages/themes/Python_For_ML_1_2/examples/tmp.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7c8843a86c5508a5740fa5c71ec987382708b23042ef322e6e48b18fbfca6a7
-size 158
+oid sha256:d00ea7ab950747878d84a2c4e9d242da96b863fa116a5c2b8d6920d4ce4cf34b
+size 67
---
+++ pages/themes/Python_For_ML_1_3/examples/tmp.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:caef20d76393b55279a8988bcc5bb2b03ab41d794a85f0c9ddcf38920498307b
+size 69
