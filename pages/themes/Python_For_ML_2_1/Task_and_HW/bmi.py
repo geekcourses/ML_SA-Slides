f7ee9034c493d93f03589a1237118b0c0358f919
geekcourses <geekcourses@gmail.com>
2022-07-19 16:53:31 +0300

    update

---
+++ pages/themes/Python_For_ML_2_1/Task_and_HW/bmi.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d12b271c40cb30a1a5779c684fcf15e505cedbb4a4b59eb117e13cb81d2e5f39
+size 809
---
+++ pages/themes/Python_For_ML_2_1/examples/tmp.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64fed6801c03a077b60f044cf905f7efeb4af7d6e48209b0b5cf3a456cfeac02
+size 203
