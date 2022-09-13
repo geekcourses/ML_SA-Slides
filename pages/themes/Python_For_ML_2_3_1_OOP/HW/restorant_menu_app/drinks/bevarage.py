b7f78279aeefde023567182746eea074ea476428
geekcourses <geekcourses@gmail.com>
2022-09-13 16:02:27 +0300

    update: day 14

--- pages/themes/Python_For_ML_2_3_1_OOP/HW/restorant_menu_app/drinks/bevarage.py
+++
@@ -1,4 +0,0 @@
-class Bevarage():
-    def __init__(name, abv, price):
-        self.name = name
-        self.abv = abv
--- pages/themes/Python_For_ML_2_3_1_OOP/examples/retrospection.py
+++
@@ -1,6 +0,0 @@
-x = 42
-y = x
-
-print( id(x) )
-print( id(y) )
-print( id(x) == id(y) )
