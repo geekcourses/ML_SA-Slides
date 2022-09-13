3330cb3d1294d3ef11b3b35c0f8926039000d016
geekcourses <geekcourses@gmail.com>
2022-09-13 21:09:50 +0300

    update

---
+++ pages/themes/Python_For_ML_2_5_OOP/examples/module.py
@@ -0,0 +1,6 @@
+def foo():
+  print("Foo")
+
+
+def bar():
+  print("Bar")
---
+++ pages/themes/Python_For_ML_2_5_OOP/examples/my_app/main.py
@@ -0,0 +1,6 @@
+# import the helper_module:
+import helper_module
+
+# use helper_module
+user_name = helper_module.get_user_name()
+helper_module.greet(user_name)
