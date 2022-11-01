bae978e09830bfcc0942d43d0afc654026ae2ae1
geekcourses <geekcourses@gmail.com>
2022-11-02 00:43:44 +0200

    update

---
+++ pages/themes/ML_Process_with_Iris_KNN_Classifier/HW/KNN_HW.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a80564c66c74b381d4907770638e70dd3e86450882754ac64ad8c8e9b7cc01f1
+size 816
---
+++ pages/themes/ML_Process_with_Iris_KNN_Classifier/HW/draw.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39590c02a786827f8732f5032bef56d787094d4b0ffedd159a5fd281074ed064
+size 1070
