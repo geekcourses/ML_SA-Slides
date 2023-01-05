6c2b7512af47334830a7659f0ecad3460ac0a368
geekcourses <geekcourses@gmail.com>
2023-01-05 17:46:43 +0200

    update

--- pages/themes/Numpy_Image_Processing/examples/black_and_wite_image.py
+++ pages/themes/Numpy_Image_Processing/examples/black_and_wite_image.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be2f00fec0d819f97161c836ea8c4993095d750199060ae0d5e5525df41dd133
-size 677
+oid sha256:c34c964b7f79c615a4b3a97ca6cfb3782c29391c703919334e13c7f76de325cd
+size 753
