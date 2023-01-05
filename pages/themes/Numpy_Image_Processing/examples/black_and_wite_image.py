eac9da46646288fe0676ff1f09cb928fed072161
geekcourses <geekcourses@gmail.com>
2023-01-05 17:46:43 +0200

    update

--- pages/themes/Numpy_Image_Processing/examples/black_and_wite_image.py
+++ pages/themes/Numpy_Image_Processing/examples/black_and_wite_image.py
@@ -2,22 +2,28 @@
 import cv2
 import numpy as np
 
-# Create a blank image with white pixels
+# Create a blank image 4x6 with white (255,255,255) pixels
 image = np.full((6, 4, 3), 255, dtype=np.uint8)
 
-# Set the pixel values for the lines of the number 0 to red (255, 0, 0)
-image[0, 0] = [255,0,0]
-# image[0, 1] = [0,0,255]
-# image[0, 2] = [0,0,255]
-# image[0, 3] = [0,0,255]
+### Set the pixel values for the lines of the number 0 to red BGR:( 0, 0, 255)
 
-# image[5, 0] = [255, 0, 0]
-# image[5, 1] = [255, 0, 0]
-# image[5, 2] = [255, 0, 0]
-# image[5, 3] = [255, 0, 0]
+## Set Top line pixels to red ()
+image[0, 0] = [0,0,255]
+image[0, 1] = [0,0,255]
+image[0, 2] = [0,0,255]
+image[0, 3] = [0,0,255]
 
-# image[1:5, 0] = [255, 0, 0]
-# image[1:5, 3] = [255, 0, 0]
+## Bottom line
+image[5, 0] = [0,0,255]
+image[5, 1] = [0,0,255]
+image[5, 2] = [0,0,255]
+image[5, 3] = [0,0,255]
+
+## Left column
+image[1:5, 0] = [0,0,255]
+
+## Right Column
+image[1:5, 3] = [0,0,255]
 
 # Save the image
 cv2.imwrite('number_0.png', image)
